b2ff246e5c866fe44fd82463ca8c9d0a6094fb5f
Nicholas Meier <nlm15@my.fsu.edu>
2020-01-26 23:34:59 -0500

    try again

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyCFfDOmsS1qGr4NsJXfhYD-5_SISXHm6Y0",
+    authDomain: "mapctf-c677b.firebaseapp.com",
+    databaseURL: "https://mapctf-c677b.firebaseio.com",
+    projectId: "mapctf-c677b",
+    storageBucket: "mapctf-c677b.appspot.com",
+    messagingSenderId: "372501642763",
+    appId: "1:372501642763:web:90a860ae080b86f52c510e",
+    measurementId: "G-Y5W2SPFFP4"
+  };
+firebase.initializeApp(firebaseConfig);
+}());
